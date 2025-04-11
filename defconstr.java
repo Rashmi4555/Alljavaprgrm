65bfe86461b00295494dc6e8d9517d862ef5c5ab
Rashmi4555 <rashmibm3579@gmail.com>
2025-04-11 09:48:26 +0530

    save

---
+++ defconstr.java
@@ -0,0 +1,11 @@
+class demo {
+    int num1;
+    int num2;
+}
+
+public class defconstr {
+    public static void main(String[] args) {
+        defconstr obj = new defconstr();
+        System.out.println("Default constructor");
+    }
+}
