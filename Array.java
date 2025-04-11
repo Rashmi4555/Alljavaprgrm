c012356b0670e1c274863898bb2fb05657af5729
Rashmi4555 <rashmibm3579@gmail.com>
2025-04-11 10:26:06 +0530

    savejavafile

---
+++ Array.java
@@ -0,0 +1,15 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println("Array elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        System.out.println("Sum of array elements: " + sum);
+    }
+}
+
